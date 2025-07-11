df9ad32d0dc79a5617e20669e1fe8770afbf0ba1
Jesus <20233tn229@utez.edu.mx>
2025-07-11 09:09:23 -0600

    Ejercicio de Asignacion de valores y correccion a codigo Variables

---
+++ Asignacion.js
@@ -0,0 +1,4 @@
+//Si hay algun valor antes de asignar a la variable se realiza el cálculo
+var a;
+a = 7;
+
---
+++ Variables.js
@@ -0,0 +1,13 @@
+var myName;
+
+/*
+Los 8 tipos de datos son 
+undefined, 
+null, 
+boolean, 
+string, 
+symbol, 
+bigint, 
+number,
+object
+*/
