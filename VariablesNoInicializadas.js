25b54beea7e46c6aa27df3411f0718e9aeea7a77
Jesus <20233tn229@utez.edu.mx>
2025-07-11 09:30:16 -0600

    Ejercicio de variables no inicializadas undefined

---
+++ VariablesNoInicializadas.js
@@ -0,0 +1,18 @@
+/*
+Cuando las variables de JavaScript son declaradas, 
+tienen un valor inicial de undefined (indefinido). 
+Si haces una operación matemática en una variable undefined,
+ tu resultado será NaN lo que significa "Not a Number" (no es un número). 
+ Si concatenas una cadena con una variable undefined, 
+obtendrás una cadena de undefined
+*/
+
+// Cambia solo el código debajo de esta línea
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Cambia solo el código encima de esta línea
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"
