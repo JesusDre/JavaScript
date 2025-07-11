0ce95d19215a11a048b5fa37b90ec28427e47205
Jesus <20233tn229@utez.edu.mx>
2025-07-11 12:57:50 -0600

    Ejercicio con la variable const

---
+++ VariableConstante.js
@@ -0,0 +1,11 @@
+/*
+const tiene todas las características increíbles que tiene let,
+con el bono añadido de que las variables declaradas usando const son de solo lectura.
+Son un valor constante, lo que significa que una vez que una variable es asignada con const,
+no se puede reasignar:
+*/
+
+const FCC = "freeCodeCamp"; // Constante que no cambia y es solo de lectura
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
