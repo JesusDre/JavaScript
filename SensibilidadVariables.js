8a19c9a2330b28d103c93d35930a41865c7a0467
Jesus <20233tn229@utez.edu.mx>
2025-07-11 10:35:25 -0600

    Ejercicio con sensibilidad en las variables camelCase

---
+++ SensibilidadVariables.js
@@ -0,0 +1,15 @@
+/*
+Escribe los nombres de las variables en JavaScript en camelCase. 
+En camelCase, los nombres de variables de múltiples palabras tienen la primera palabra en minúsculas 
+y la primera letra de cada palabra posterior en mayúsculas.
+*/
+
+// Declaración de variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Asignación de variables
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
