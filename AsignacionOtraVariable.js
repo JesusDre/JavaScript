c4c29a38c06317f8cc149872cb5c17825ec96f77
Jesus <20233tn229@utez.edu.mx>
2025-07-11 09:13:45 -0600

    Ejercicio asignacion de valor a otra variable

---
+++ AsignacionOtraVariable.js
@@ -0,0 +1,5 @@
+
+var a; //Se crea la variable 'a'
+a = 7; //Se asigna el valor de 7 a la variable 'a'
+var b;//Se crea la variable 'b'
+b = a;//Se asigna a la variable 'b' el valor 7 que contiene la variable 'a'
