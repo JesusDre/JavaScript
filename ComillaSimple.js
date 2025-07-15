05319a47d2a7805bbd34dc7ff12c88497beb40f3
Jesus <20233tn229@utez.edu.mx>
2025-07-15 10:07:49 -0600

    Ejercicio con commilla simple

---
+++ ComillaSimple.js
@@ -0,0 +1,16 @@
+/*
+A diferencia de otros lenguajes de programación, 
+las comillas simples y dobles funcionan igual en JavaScript.
+*/
+//Mal
+const myStr2 = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+//Bien
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+/*Nota
+ La barra invertida \ 
+ no debe confundirse con 
+ la barra diagonal /.
+  No hacen lo mismo.
+*/
