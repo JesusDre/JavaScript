18beaa7319f9a2186cdf6f31ee24654689892eea
Jesus <20233tn229@utez.edu.mx>
2025-07-15 10:16:57 -0600

    Ejecicio con secuencia de escape barra invertida

---
+++ SecuenciaEscape.js
@@ -0,0 +1,19 @@
+/*
+\'	comilla simple
+\"	comilla doble
+\\	barra invertida
+\n	línea nueva
+\t	tabulador
+\r	retorno del carro
+\b	retroceso
+\f	fuente de formulario
+Ten en cuenta que la barra invertida debe escaparse para que aparezca como tal.
+*/
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+
+//Resultado
+/*
+FirstLine
+    \SecondLine
+ThirdLine
+*/
