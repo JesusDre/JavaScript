37161b33108c0797094349beeee76deb0bbff168
Jesus <20233tn229@utez.edu.mx>
2025-07-15 13:21:13 -0600

    Ejercicio para encontrar el ultimo caracter .length -1

---
+++ EncontrarUltimoCaracter.js
@@ -0,0 +1,5 @@
+//En lugar de contar todos los caracteres a partir del 0
+//podemos utilizar .length -1 para contar el ultimo
+
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length -1]; 
