f7b933b6c2a3354ef00f59ebf0f606f303dabd12
Jesus <20233tn229@utez.edu.mx>
2025-07-15 14:40:55 -0600

    Ejercicio con la variable de arreglo

---
+++ Arreglos.js
@@ -0,0 +1,6 @@
+/*
+Con las variables de arreglos (array) de JavaScript,
+ podemos almacenar varios datos en un solo lugar.
+*/
+//Se pueden agregar cadenas y enteros etc. 
+const myArray = ["Milk", 5];
