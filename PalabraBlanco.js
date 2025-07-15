4c9969f8e60d54cd2993ae8823a46bbb9c6c5fe5
Jesus <20233tn229@utez.edu.mx>
2025-07-15 13:38:09 -0600

    Ejercicio de concatenacion de variables y cadenas

---
+++ PalabraBlanco.js
@@ -0,0 +1,18 @@
+/*
+1. wordBlanks debe ser una cadena.
+2. No debes cambiar los valores asignados
+a myNoun, myVerb, myAdjective o myAdverb.
+3. No debes utilizar directamente los valores
+dog, ran, big o quickly para crear wordBlanks.
+4. wordBlanks debes contener todas las palabras
+asignadas a las variables myNoun, myVerb, myAdjective y myAdverb
+separadas por caracteres no-palabra 
+(y cualquier palabra adicional de su elección).
+*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+//It was really big, my dog and ran quickly
+const wordBlanks = "It was really " + myAdjective + " my " + myNoun + " and " +  myVerb + " " + myAdverb; 
