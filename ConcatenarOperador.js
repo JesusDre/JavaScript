b125cecbbe00009b266b44639b56d32808423d01
Jesus <20233tn229@utez.edu.mx>
2025-07-15 10:36:31 -0600

    Ejercicio concatenacion con operador +=

---
+++ ConcatenarOperador.js
@@ -0,0 +1,7 @@
+/*
+También podemos utilizar el operador +=
+para concatenar una cadena al final de una variable de cadena existente.
+*/
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence."
+//Esto puede ser muy útil para romper una cadena larga en varias líneas.
