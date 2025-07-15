aa02e5c4399fee263f943a3f4d1387b10c29c717
Jesus <20233tn229@utez.edu.mx>
2025-07-15 15:04:24 -0600

    Ejercicio con arreglo mutable en un indice especifico

---
+++ ArreglosMutables.js
@@ -0,0 +1,7 @@
+/*
+A diferencia de las cadenas, 
+las entradas de los arreglos son mutables y pueden cambiarse libremente, 
+incluso si el arreglo fue declarado con const.
+*/
+const myArray = [18, 64, 99];
+myArray[0] = 45;
