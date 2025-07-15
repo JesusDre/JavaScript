387f5d12124ba77d64af812a26d4953a65c34a51
Jesus <20233tn229@utez.edu.mx>
2025-07-15 10:26:37 -0600

    Ejercicio de concatenacion

---
+++ Concatenar.js
@@ -0,0 +1,9 @@
+
+/*
+cuando el operador + se utiliza con un valor de cadena (String), 
+se le llama operador de concatenación.
+Nota: Ten cuidado con los espacios.
+La concatenación no añade espacios entre las cadenas concatenadas, 
+así que tendrás que añadirlos por tu cuenta.
+*/
+const myStr = "This is the start." + " This is the end."; 
