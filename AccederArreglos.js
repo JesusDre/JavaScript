05a07905a126ae1e30401dc4ff244bc8dcbc85af
Jesus <20233tn229@utez.edu.mx>
2025-07-15 14:50:04 -0600

    Ejercicio para acceder a arreglos con indices[0]

---
+++ AccederArreglos.js
@@ -0,0 +1,11 @@
+/*
+Podemos acceder a los datos dentro de un arreglo usando índices.
+Los índices de los arreglos se escriben en la misma notación de corchetes
+que usan las cadenas de texto, con la excepción que en vez
+de especificar un carácter, se especifica una entrada en el arreglo.
+Así como las cadenas de texto, 
+los arreglos usan indexación empezando desde cero,
+en donde el primer elemento de un arreglo tiene como índice 0.
+*/
+const myArray = [50, 60, 70];
+let myData = myArray[0]
