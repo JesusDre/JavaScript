21165cce87b106379104591c3429f4437e94f770
Jesus <20233tn229@utez.edu.mx>
2025-07-15 11:57:13 -0600

    Ejercicio con .length para determinar longitud

---
+++ LongitudCadena.js
@@ -0,0 +1,9 @@
+
+/*Puedes encontrar la longitud de un valor de cadena (String) escribiendo 
+.length después de la variable de cadena o literal de cadena.
+*/ 
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
