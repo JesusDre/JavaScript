a01dcb8539cf9fa4bc713cdef0fa024d3e1dd2cd
Jesus <20233tn229@utez.edu.mx>
2025-07-15 12:05:43 -0600

    Ejercicio para obtener caracter en una cadena [0]

---
+++ NotacionCorchetes.js
@@ -0,0 +1,10 @@
+/*
+La notación de corchetes es una 
+forma de obtener un carácter en un índice (index)
+específico dentro de una cadena.
+*/
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
