52eed7d788fac71023b2966474248126957a9c3e
Jesus <20233tn229@utez.edu.mx>
2025-07-15 12:27:40 -0600

    Ejercicio con inmutabilidad de las Cadenas

---
+++ InmutabilidadCadenas.js
@@ -0,0 +1,12 @@
+/*
+los valores de cadena (String) son inmutables,
+lo que significa que no pueden ser alterados una vez creados.
+*/
+// Configuración
+let myStr = "Jello World";
+
+//Mal
+myStr[0] = "H";
+///Bien
+myStr = "Hello World"; 
+
