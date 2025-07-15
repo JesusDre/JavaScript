011fec52b16da313675fcf12d5c7c9ceec34f7fa
Jesus <20233tn229@utez.edu.mx>
2025-07-15 15:46:51 -0600

    Ejercicio con el metodo push en arreglo

---
+++ MetodoPush.js
@@ -0,0 +1,8 @@
+/*
+El método push() toma uno o más arguments y los añade al final del arreglo,
+en el orden en que aparecen. 
+Devuelve la nueva longitud del arreglo.
+*/
+// Empuja ["dog", 3] al final de la variable myArray.
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
